6e027ce623be33a0324a5d1e2e1c3babe16edf7c
Vasilina Biznyakova <vbiznyakova@gmail.com>
2022-08-19 15:00:57 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,17 @@
 // 1) napisat tajmer obratnogo otscheta
 // 2) poluchit nachalnuju daty iz polja start date.
 
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
 const daysEl = document.querySelector('[data-days]');
 const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
-const startDateEl = document.querySelector("#datetime-picker");
+const startDateEl = document.querySelector('#datetime-picker');
 const startBtnEl = document.querySelector('[data-start]');
 
-startBtnEl.setAttribute("disabled", true);
+startBtnEl.setAttribute('disabled', true);
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -20,41 +19,38 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0].getTime() >= options.defaultDate.getTime()) {
-      startBtnEl.removeAttribute ('disabled');
+      startBtnEl.removeAttribute('disabled');
     } else {
-      window.alert("Please choose a date in the future");
+      window.alert('Please choose a date in the future');
     }
-    
-   },
-
-  
+  },
 };
 
-flatpickr("#datetime-picker", options);
+flatpickr('#datetime-picker', options);
 
-function timer (){
+function timer() {
   const currentDate = new Date();
-    const startDate = new Date(startDateEl.value);
-    const difference = startDate - currentDate;
- 
-    const { days, hours, minutes, seconds } = convertMs(difference);
-    
-    daysEl.textContent = days;
-    hoursEl.textContent = hours;
-    minutesEl.textContent = minutes;
-    secondsEl.textContent = seconds;
+  const startDate = new Date(startDateEl.value);
+  const difference = startDate - currentDate;
+
+  const { days, hours, minutes, seconds } = convertMs(difference);
+
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
 }
 
 startBtnEl.addEventListener('click', () => {
-intervalId = setInterval( timer, 1000);
-if (difference = 0) {
-  clearInterval(intervalId);
-}
-} );
+  const intervalId = setInterval(timer, 1000);
+  if ((difference = 0)) {
+    clearInterval(intervalId);
+  }
+});
 
 function addLeadingZero(value) {
-  return String(value).padStart(2, "0");
-};
+  return String(value).padStart(2, '0');
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -70,8 +66,9 @@ function convertMs(ms) {
   // Remaining minutes
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
 }
-
